854f80c4d0d42e62b80e95e34ab532dafc415cc2
Ramya <emailtoramya@gmail.com>
2015-11-30 05:22:47 -0500

    Test Cases without Code implementation

--- order.py
+++ order.py
@@ -11,6 +11,8 @@ class Order:
         if not len(inputstring) == 39:
             raise ValueError("input string is not valid")
 
+    def create(self):
+        return self
 
 
 
--- test_order.py
+++ test_order.py
@@ -13,6 +13,41 @@ class TestOrder(unittest.TestCase):
         self.assertRaises(ValueError, Order, "Hello World")
         self.assertRaises(ValueError, Order, "HelloWorldHelloWorldHelloWorldHelloWorld")
 
+    def test_validateLineNoDataType(self):
+        self.assertRaises(TypeError, Order, " a10 P15678923   SPORTING856    1012399")
+
+    def test_checkLineNoIsSet(self):
+        o = Order(" 100 P15678923   SPORTING856    1012399")
+        self.assertEqual(self, o.create()._lineNo, 100)
+
+    def test_validateLineProductCodeDataType(self):
+        self.assertRaises(TypeError, Order, " 100 015678923   SPORTING856    1012399")
+
+    def test_checkProductCodeIsSet(self):
+        o = Order(" 100 P15678923   SPORTING856    1012399")
+        self.assertEqual(self, o.create()._code, "P15678923")
+
+    def test_validateCategoryDataType(self):
+        self.assertRaises(TypeError, Order, " 100 P15678923   SPORTING856    1012399")
+
+    def test_checkCategoryIsSet(self):
+        o = Order(" 100 P15678923   SPORTING856    1012399")
+        self.assertEqual(self, o.create()._category, "SPORTING")
+
+    def test_validateQuantityDataType(self):
+        self.assertRaises(TypeError, Order, " 100 P15678923  SPORTING856     10AB399")
+
+    def test_checkProductQuantityIsSet(self):
+        o = Order(" 100 P15678923   SPORTING856    1012399")
+        self.assertEqual(self, o.create()._quantity, 856)
+
+    def test_validatePriceDataType(self):
+        self.assertRaises(TypeError, Order, " 100 015678923 SPORTING99856    1012399")
+
+    def test_checkProductPriceIsSet(self):
+        o = Order(" 100 P15678923 SPORTING99856    1012399")
+        self.assertEqual(self, o.create()._category, 10123.99)
+
 if __name__ == "__main__":
     unittest.main()
 
