d6fb9e39110433a492a195f0d552f8b6b5b183d6
Ramya <emailtoramya@gmail.com>
2015-12-01 00:09:45 -0500

    Refactor - Refactored Order Class to replace hard coded values with attribute lengths.

--- order.py
+++ order.py
@@ -6,33 +6,40 @@ class Order:
 
     def __init__(self, inputstring):
 
+        inputLen = 39
+        lNumLen = 4
+        codeLen = 10
+        cateLen = 10
+        quanLen = 5
+        priceLen = 10
+
         if not inputstring:
             raise ValueError("Input string is empty")
 
-        if not len(inputstring) == 39:
+        if not len(inputstring) == inputLen:
             raise ValueError("input string is not valid")
 
-        lineNo = inputstring[:4].strip()
+        lineNo = inputstring[:lNumLen].strip()
         if not lineNo.isdigit():
             raise TypeError("Invalid Input - LineNo is not integer")
         self._lineNo = int(lineNo)
 
-        prodCode = inputstring[4:14].strip()
+        prodCode = inputstring[lNumLen:(lNumLen+codeLen)].strip()
         if not prodCode.isalnum():
             raise TypeError("Invalid Input - Product Code is not alphanumeric")
         self._code = prodCode
 
-        category = inputstring[14:24].strip()
+        category = inputstring[(lNumLen+codeLen):(lNumLen+codeLen+cateLen)].strip()
         if not category.isalnum():
             raise TypeError("Invalid Input - Category is not alphanumeric")
         self._category = category
 
-        quantity = inputstring[24:28].strip()
+        quantity = inputstring[(lNumLen+codeLen+cateLen):(lNumLen+codeLen+cateLen+quanLen)].strip()
         if not quantity.isdigit():
             raise TypeError("Invalid Input - Quantity is not numeric")
         self._quantity = int(quantity)
 
-        price = inputstring[29:39].strip()
+        price = inputstring[(lNumLen+codeLen+cateLen+quanLen):inputLen].strip()
         if not price.isdigit():
             raise TypeError("Invalid Input - Price is not numeric")
 
