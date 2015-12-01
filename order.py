e8ac9f994b427c5ab472eea2d420fe4f5bc96564
Ramya <emailtoramya@gmail.com>
2015-12-01 00:13:51 -0500

    Refactored to adher to Python naming conventions

--- order.py
+++ order.py
@@ -6,40 +6,40 @@ class Order:
 
     def __init__(self, inputstring):
 
-        inputLen = 39
-        lNumLen = 4
-        codeLen = 10
-        cateLen = 10
-        quanLen = 5
-        priceLen = 10
+        inputlen = 39
+        linenumlen = 4
+        codelen = 10
+        catelen = 10
+        quanlen = 5
+        pricelen = 10
 
         if not inputstring:
             raise ValueError("Input string is empty")
 
-        if not len(inputstring) == inputLen:
+        if not len(inputstring) == inputlen:
             raise ValueError("input string is not valid")
 
-        lineNo = inputstring[:lNumLen].strip()
-        if not lineNo.isdigit():
+        linenum = inputstring[:linenumlen].strip()
+        if not linenum.isdigit():
             raise TypeError("Invalid Input - LineNo is not integer")
-        self._lineNo = int(lineNo)
+        self._lineNo = int(linenum)
 
-        prodCode = inputstring[lNumLen:(lNumLen+codeLen)].strip()
-        if not prodCode.isalnum():
+        prodcode = inputstring[linenumlen:(linenumlen+codelen)].strip()
+        if not prodcode.isalnum():
             raise TypeError("Invalid Input - Product Code is not alphanumeric")
-        self._code = prodCode
+        self._code = prodcode
 
-        category = inputstring[(lNumLen+codeLen):(lNumLen+codeLen+cateLen)].strip()
+        category = inputstring[(linenumlen+codelen):(linenumlen+codelen+catelen)].strip()
         if not category.isalnum():
             raise TypeError("Invalid Input - Category is not alphanumeric")
         self._category = category
 
-        quantity = inputstring[(lNumLen+codeLen+cateLen):(lNumLen+codeLen+cateLen+quanLen)].strip()
+        quantity = inputstring[(linenumlen+codelen+catelen):(linenumlen+codelen+catelen+quanlen)].strip()
         if not quantity.isdigit():
             raise TypeError("Invalid Input - Quantity is not numeric")
         self._quantity = int(quantity)
 
-        price = inputstring[(lNumLen+codeLen+cateLen+quanLen):inputLen].strip()
+        price = inputstring[(linenumlen+codelen+catelen+quanlen):inputlen].strip()
         if not price.isdigit():
             raise TypeError("Invalid Input - Price is not numeric")
 
